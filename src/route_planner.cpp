8d859942fa1cf7d0551736802aaf0174dfdc732d
raisulsgk <Raisul.Islam@sgkinc.com>
2022-06-02 15:20:57 +0200

    TODO 2

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -11,6 +11,9 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
     // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
     // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
 
+    float start_x, start_y, end_x, end_y;
+    std::cin >> start_x >> start_y >> end_x >> end_y;
+
 }
 
 
